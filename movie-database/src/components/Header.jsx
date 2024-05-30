980236027ced35cea734823958921d5398ba9970
Gillian <gillianldowney12@gmail.com>
2024-05-29 22:55:47 -0700

    dicked around a bit but got no where

--- movie-database/src/components/Header.jsx
+++ movie-database/src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Nav from './Nav'; // Import Nav component
+import neuvieLogo from '../assets/neuvie-logo.svg';
 import '../scss/styles.scss';
 
-const Header = ({ title }) => {
+const Header = () => {
   console.log("Header component rendered");
   return (
     <header className="header">
       <h1 className="header__title">
-        <Link to="/" className="header__title-link">{title}</Link>
+        <Link to="/" className="header__title-link">
+          <img src={neuvieLogo} alt="Neuvie Logo" className='header__neuvie-logo'/>
+        </Link>
       </h1>
       <Nav /> {/* Include Nav component here */}
     </header>
